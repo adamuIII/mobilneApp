7501dfbdbac05478214430b068a08e90564addcb
adamuIII <adamrzepka32167@gmail.com>
2021-11-23 02:29:39 +0100

    Podstawy strony startowej
    Do zarządzania podstronami używamy NavigationContainer

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from './components/auth/Landing';
+
+
+// const Stack bedzue przechowywac sceny naszej aplikacji
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ components/auth/Landing.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {StyleSheet,Text,View,Button,ImageBackground } from 'react-native'
+import backgroundImage from '../../assets/background.png'
+
+
+export default function Landing(navigation) {
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require=(backgroundImage)} resizeMode="cover" style={styles.image}>
+            </ImageBackground>
+            <Button
+            color="#313ab0"
+            title="Login"
+            onPress={()=>navigation.navigate("Login")}/>
+            <Button
+            color="#3295a8"
+            title="Register"
+            onPress={()=>navigation.navigate("Register")}/>
+            
+            
+        </View>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1, 
+        justifyContent: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        flex: 1 
+      },
+
+
+
+})
